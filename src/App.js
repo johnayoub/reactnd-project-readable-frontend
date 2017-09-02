9cac4dcdac2c094db5775ea9b235adb40a9c30d5
John Ayoub <john.a.ayoub@gmail.com>
2017-09-02 10:57:35 -0400

    Wrap the routes inside a Switch component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import CategoryView from "./components/CategoryView";
 import { connect } from 'react-redux';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import '@material/typography/dist/mdc.typography.css';
 import './App.css';
@@ -17,7 +17,9 @@ class App extends Component {
                         <Header/>
                     </div>
                     <div className="app-body">
-                        <Route exact path="/:category?" component={CategoryView}/>
+                        <Switch>
+                            <Route exact path="/:category?" component={CategoryView}/>
+                        </Switch>
                     </div>
                 </div>
             </MuiThemeProvider>
